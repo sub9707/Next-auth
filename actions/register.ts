eb4120aeefe81d1eb0c3d7ff5948865fcc7ab152
김승섭 <sub9707@naver.com>
2024-01-19 07:08:28 +0900

    feat : 회원가입 로직 추가

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { getUserByEmail } from "@/data/user";
 
 // 회원가입 서버 액션
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -10,5 +13,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "회원가입 정보를 확인해주세요." };
   }
 
-  return { success: "전송 완료" };
+  const { email, name, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // 가입된 유저정보 체크
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "이미 존재하는 이메일입니다." };
+  }
+
+  // 유저 생성
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "회원가입 완료" };
 };
