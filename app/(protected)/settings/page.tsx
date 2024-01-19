3e50fd4e52ae47f22dba5a53f294ddff2f0c3f5a
김승섭 <sub9707@naver.com>
2024-01-20 06:07:56 +0900

    feat : session - user.role 타입 추가

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 async function page() {
   const session = await auth();
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}>
+        <button type="submit">로그아웃</button>
+      </form>
+    </div>
+  );
 }
 
 export default page;
--- auth.ts
+++ auth.ts
@@ -1,8 +1,19 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 
 import { db } from "@/lib/db";
+import { getUserById } from "@/data/user";
+
+// user role type 선언 -> d.ts로 구분 가능
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: "ADMIN" | "USER";
+    } & DefaultSession["user"];
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +21,36 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async signIn({ user }) {
+      const existUser = await getUserById(user.id || "");
+
+      if (!existUser || !existUser.emailVerified) {
+        return false;
+      }
+      return true;
+    },
+    //@ts-ignore
+    async session({ token, session }) {
+      console.log({ sessionToken: token });
+      if (token && token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existUser = await getUserById(token.sub);
+      if (!existUser) return token;
+
+      token.role = existUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
