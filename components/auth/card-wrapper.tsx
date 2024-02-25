194cebcc5e9d5d2ddfefd8310ac44d8777a1e0e5
김승섭 <sub9707@naver.com>
2024-02-26 06:06:48 +0900

    [fix] - 테스트용 로그인 추가

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -9,6 +9,8 @@ import {
 import Header from "./header";
 import Social from "./social";
 import BackButton from "./back-button";
+import { startTransition } from "react";
+import { login } from "@/actions/login";
 
 type CardWrapperProps = {
   children: React.ReactNode;
@@ -25,8 +27,20 @@ function CardWrapper({
   backButtonHref,
   showSocial,
 }: CardWrapperProps) {
+  const testLogin = () => {
+    let values = {
+      email: "admin@admin.com",
+      password: "adminadmin",
+    };
+    startTransition(() => {
+      login(values).then((data) => {
+        //@ts-ignore
+        setSuccess(data?.success);
+      });
+    });
+  };
   return (
-    <Card className="w-[400px] shadow-md">
+    <Card className="w-[400px] shadow-md bg-white dark:bg-gray-900">
       <CardHeader>
         <Header label={headerLabel} />
       </CardHeader>
@@ -36,8 +50,13 @@ function CardWrapper({
           <Social />
         </CardFooter>
       )}
-      <CardFooter>
+      <CardFooter className="block">
         <BackButton label={backButtonLabel} href={backButtonHref} />
+        <p
+          className="text-center mt-3 text-sm cursor-pointer"
+          onClick={testLogin}>
+          로그인없이 사용
+        </p>
       </CardFooter>
     </Card>
   );
